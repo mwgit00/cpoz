fd84b5fc14900a1b827e15d406da4fc079faff64
Mark Whitney <mwgit00@earthlink.net>
2019-10-12 13:42:37 -0500

    Cam loop
    Added camera loop in main application.  It will be able to find BGRLandmarks and do calibration stuff.  Eventually, it may do live landmark triangulation.

---
+++ BGRLandmark.h
@@ -0,0 +1,182 @@
+// MIT License
+//
+// Copyright(c) 2019 Mark Whitney
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef BGR_LANDMARK_H_
+#define BGR_LANDMARK_H_
+
+// uncomment line below to collect up to 1000 samples and write them to an image file when done
+//#define _COLLECT_SAMPLES
+
+#include <map>
+#include "opencv2/imgproc.hpp"
+
+
+namespace cpoz
+{
+    class BGRLandmark
+    {
+    public:
+
+        typedef struct
+        {
+            cv::Point ctr;      // center point of landmark
+            double diff;        // value of positive template match minus negative template match
+            double rng;         // range of pixels in candidate ROI
+            double min;         // min pixel in candidate ROI
+            int code;           // color code, -1 for unknown, else 0-11
+        } landmark_info_t;
+
+        // names of colors with 0 or 255 as the BGR components
+        enum class bgr_t : int
+        {
+            BLACK,
+            RED,
+            GREEN,
+            YELLOW,
+            BLUE,
+            MAGENTA,
+            CYAN,
+            WHITE
+        };
+
+        // codes for the color of each block in 2x2 grid (clockwise from upper left)
+        typedef struct _T_grid_colors_struct
+        {
+            bgr_t c00;
+            bgr_t c01;
+            bgr_t c11;
+            bgr_t c10;
+        } grid_colors_t;
+
+        // there are 8 colors that only have 0 or 255 as the BGR components
+        static const cv::Scalar BGR_COLORS[8];
+        static const cv::Scalar BGR_BORDER;
+
+        // the supported landmark color patterns
+        static const std::map<char, grid_colors_t> PATTERN_MAP;
+
+    public:
+
+        BGRLandmark();
+        virtual ~BGRLandmark();
+
+        // init with "good" default settings
+        void init(
+            const int k = 9,
+            const double thr_corr = 1.6,    // threshold for dual match (range is 0.0 to 2.0)
+            const int thr_pix_rng = 40,     // grey image pixel range threshold for pre-proc
+            const int thr_pix_min = 70,     // grey image dark pixel threshold for pre-proc
+            const int thr_bgr_rng = 20);    // range in BGR required for color matching step
+
+        // runs the match on an original BGR image and possibly pre-processed gray image
+        // it returns a gray image with the raw template match and a vector of landmark info
+        void perform_match(
+            const cv::Mat& rsrc_bgr,
+            const cv::Mat& rsrc,
+            cv::Mat& rtmatch,
+            std::vector<BGRLandmark::landmark_info_t>& rpts);
+
+        // gets centering offset for the landmark template
+        const cv::Point& get_template_offset(void) const { return tmpl_offset; }
+
+        // normally color ID should always be enabled
+        // but it can be turned off for testing
+        void set_color_id_enable(const bool f) { is_color_id_enabled = f; }
+
+
+        // creates printable 2x2 landmark image
+        static void create_landmark_image(
+            cv::Mat& rimg,
+            const double dim_grid = 1.0,
+            const double dim_border = 0.25,
+            const grid_colors_t& rcolors = { bgr_t::BLACK, bgr_t::WHITE, bgr_t::BLACK, bgr_t::WHITE },
+            const cv::Scalar border_color = BGR_BORDER,
+            const int dpi = 96);
+
+        // creates printable multi-landmark image by repeating 2x2 landmark patterns
+        // they are placed in row-major order in the image based on the repeat counts
+        // the labels identify each landmark, cycling back around if necessary
+        // it's up to the user to pick sane dimensions and repeat counts
+        static void create_multi_landmark_image(
+            cv::Mat& rimg,
+            const std::string& rslabels,
+            const int xrepeat,
+            const int yrepeat,
+            const double dim_grid = 1.0,
+            const double dim_spacing = 2.0,
+            const double dim_border = 0.25,
+            const cv::Scalar border_color = BGR_BORDER,
+            const int dpi = 96);
+
+        static int compare_by_code(BGRLandmark::landmark_info_t& a, BGRLandmark::landmark_info_t& b)
+        {
+            return a.code < b.code;
+        }
+
+    private:
+
+        // creates a 2x2 grid BGR template of pixel dimension k
+        static void create_template_image(
+            cv::Mat& rimg,
+            const int k,
+            const grid_colors_t& rcolors);
+
+        // takes landmark info and snapshot of landmark
+        // and tries to identify the colors in the non-black squares
+        void identify_colors(const cv::Mat& rimg, BGRLandmark::landmark_info_t& rinfo) const;
+
+        // converts the "sign" of the landmark and its 2 bright colors into a single code
+        static int get_bgr_code(double s, const int a, const int b);
+
+
+    private:
+
+        // size of square landmark template
+        int kdim;
+
+        // thresholds for match consideration
+        double thr_corr;
+        int thr_pix_rng;
+        int thr_pix_min;
+        int thr_bgr_rng;
+
+        // templates for 2x2 checkerboard grid
+        cv::Mat tmpl_gray_p;
+        cv::Mat tmpl_gray_n;
+
+        // offset for centering template location
+        cv::Point tmpl_offset;
+
+        // flag for controlling color ID function
+        bool is_color_id_enabled;
+
+#ifdef _COLLECT_SAMPLES
+    public:
+        const int sampx = 40;
+        const int sampy = 25;
+        int samp_ct;
+        cv::Mat samples;
+#endif
+    };
+}
+
+#endif // BGR_LANDMARK_H_
--- cpoz.cpp
+++ cpoz.cpp
@@ -21,12 +21,38 @@
 // SOFTWARE.
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <set>
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/videoio.hpp"
 
 #include "room_test.h"
 #include "room1.h"
 #include "room2.h"
-//#include "CameraHelper.h"
-//#include "XYZLandmark.h"
+#include "BGRLandmark.h"
+
+using namespace cv;
+
+#define CAL_PATH    ".\\calib\\"    // user may need to create or change this
+#define MOVIE_PATH  ".\\movie\\"    // user may need to create or change this
+#define DATA_PATH   ".\\data\\"     // user may need to change this
+
+#define SCA_BLACK   (cv::Scalar(0,0,0))
+#define SCA_RED     (cv::Scalar(0,0,255))
+#define SCA_GREEN   (cv::Scalar(0,255,0))
+#define SCA_YELLOW  (cv::Scalar(0,255,255))
+#define SCA_BLUE    (cv::Scalar(255,0,0))
+
+
+static bool is_rec_enabled = false;
+static bool is_cal_enabled = false;
+const char * stitle = "CPOZ Test Application";
+int n_record_ctr = 0;
 
 
 void test_room1()
@@ -39,7 +65,10 @@ void test_room1()
     {
         // LM name mapped to [world_azim, elev] for visibility at world (1, 1)
         cv::Point3d world_xyz = { 1.0, -2.0, 1.0 };
-        room_test(room1::landmark_maps, room1::lm_vis_1_1, world_xyz, "mark1", "mark2");  // one case with one landmark not visible
+        
+        // this test has one case with one landmark not visible
+        room_test(room1::landmark_maps, room1::lm_vis_1_1, world_xyz, "mark1", "mark2");
+        
         room_test(room1::landmark_maps, room1::lm_vis_1_1, world_xyz, "mark1", "mark3");
         room_test(room1::landmark_maps, room1::lm_vis_1_1, world_xyz, "mark1", "mark2", 10.0);
         room_test(room1::landmark_maps, room1::lm_vis_1_1, world_xyz, "mark1", "mark3", 10.0);
@@ -61,6 +90,7 @@ void test_room1()
 
     show_tally();
 }
+
 
 void test_room2()
 {
@@ -89,11 +119,219 @@ void test_room2()
 }
 
 
+bool wait_and_check_keys()
+{
+    bool result = true;
+
+    int nkey = waitKey(1);
+    char ckey = static_cast<char>(nkey);
+
+    // check that a keypress has been returned
+    if (nkey >= 0)
+    {
+        if (ckey == 27)
+        {
+            // done if ESC has been pressed
+            result = false;
+        }
+        else
+        {
+            switch (ckey)
+            {
+            case 'c': is_cal_enabled = !is_cal_enabled; break;
+            case 'r': is_rec_enabled = !is_rec_enabled; break;
+            default: break;
+            }
+        }
+    }
+
+    return result;
+}
+
+
+void image_output(cv::Mat& rimg)
+{
+    // save each frame to a file if recording
+    if (is_rec_enabled)
+    {
+        std::ostringstream osx;
+        osx << MOVIE_PATH << "img_" << std::setfill('0') << std::setw(5) << n_record_ctr << ".png";
+        imwrite(osx.str(), rimg);
+        n_record_ctr++;
+
+        // red box in upper corner if recording
+        rectangle(rimg, { 0, 0, 4, 4 }, SCA_RED, -1);
+    }
+
+    imshow(stitle, rimg);
+}
+
+
+void loop_cal(void)
+{
+    const int max_good_ct = 20;
+
+    cv::FileStorage cvfs;
+    std::vector<std::vector<cv::Vec2f>> vvcal;
+    std::vector<std::string> vcalfiles;
+    std::set<int> cal_label_set;
+    int cal_good_ct;
+    int cal_ct;
+
+    Size capture_size;
+    Point ptmax;
+
+    Mat img_viewer;
+    Mat img_gray;
+    Mat tmatch;
+
+    cpoz::BGRLandmark bgrm;
+
+    // need a 0 as argument
+    VideoCapture vcap(0);
+    if (!vcap.isOpened())
+    {
+        std::cout << "Failed to open VideoCapture device!" << std::endl;
+        ///////
+        return;
+        ///////
+    }
+
+    // camera is ready so grab a first image to determine its full size
+    vcap >> img_viewer;
+    capture_size = img_viewer.size();
+
+    cvfs.open("cal_meta.yaml", cv::FileStorage::WRITE);
+
+    // and the image processing loop is running...
+    bool is_running = true;
+
+    while (is_running)
+    {
+        // grab image
+        vcap >> img_viewer;
+
+        // combine all channels into grayscale
+        cvtColor(img_viewer, img_gray, COLOR_BGR2GRAY);
+
+        // look for landmarks
+        std::vector<cpoz::BGRLandmark::landmark_info_t> qinfo;
+        bgrm.perform_match(img_viewer, img_gray, tmatch, qinfo);
+
+#ifdef _COLLECT_SAMPLES
+        std::cout << bgrm.samp_ct << std::endl;
+#endif
+
+        if (true)
+        {
+            // draw circles around all BGR landmarks and put labels by each one
+            // unless about to snap a calibration image which can't have the circles
+            cal_label_set.clear();
+            for (const auto& r : qinfo)
+            {
+                cal_label_set.insert(r.code);
+                if ((cal_good_ct < (max_good_ct - 3)) || (cal_good_ct >= max_good_ct))
+                {
+                    char x[2] = { 0 };
+                    x[0] = static_cast<char>(r.code) + 'A';
+                    circle(img_viewer, r.ctr, 7, (r.diff > 0.0) ? SCA_RED : SCA_BLUE, 3);
+                    putText(img_viewer, std::string(x), r.ctr, FONT_HERSHEY_PLAIN, 2.0, SCA_GREEN, 2);
+                }
+            }
+
+            // in this loop, use mask flag as calibration grab mode
+            // the image is dumped to file if 12 unique landmarks found (3x4 pattern)
+            // then user must "hide" some landmarks to trigger another grab
+            if (is_cal_enabled)
+            {
+                if (cal_label_set.size() == 12)
+                {
+                    if (cal_good_ct < max_good_ct)
+                    {
+                        cal_good_ct++;
+                        if (cal_good_ct == max_good_ct)
+                        {
+                            // save snapshot of image
+                            std::ostringstream osx;
+                            osx << MOVIE_PATH << "img_" << std::setfill('0') << std::setw(5) << cal_ct << ".png";
+                            std::string sfile = osx.str();
+                            imwrite(sfile, img_viewer);
+                            vcalfiles.push_back(sfile);
+                            std::cout << "CALIB. SNAP " << sfile << std::endl;
+
+                            // sort image points by label code
+                            std::vector<cv::Vec2f> vimgpts;
+                            std::sort(qinfo.begin(), qinfo.end(), cpoz::BGRLandmark::compare_by_code);
+                            for (const auto& r : qinfo)
+                            {
+                                vimgpts.push_back(cv::Vec2f(
+                                    static_cast<float>(r.ctr.x),
+                                    static_cast<float>(r.ctr.y)));
+                            }
+                            vvcal.push_back(vimgpts);
+                            cal_ct++;
+                        }
+                    }
+                }
+                else
+                {
+                    cal_good_ct = 0;
+                }
+            }
+            else
+            {
+                // calibration mode turned off
+                cal_good_ct = 0;
+                cal_ct = 0;
+            }
+        }
+
+        // always show best match contour and target dot on BGR image
+        image_output(img_viewer);
+
+        // handle keyboard events and end when ESC is pressed
+        is_running = wait_and_check_keys();
+    }
+
+    // when everything is done, release the capture device and windows
+    vcap.release();
+    cv::destroyAllWindows();
+
+    // dump cal data if still in cal mode
+    if (is_cal_enabled)
+    {
+        // the BGRLandmark calibration pattern has 12 corners A-L in ordering shown below
+        // so the corners array must be initialized in same order
+        // A D G J
+        // B E H K
+        // C F I L
+        std::vector<Point3f> vgridpts;
+        double grid_square = 2.25;
+        Size board_size(4, 3);
+        for (int j = 0; j < board_size.width; j++)
+        {
+            for (int i = 0; i < board_size.height; i++)
+            {
+                vgridpts.push_back(cv::Point3f(float(j * grid_square), float(i * grid_square), 0));
+            }
+        }
+        cvfs << "image_size" << capture_size;
+        cvfs << "grid_size" << board_size;
+        cvfs << "grid_square" << grid_square;
+        cvfs << "grid_pts" << vgridpts;
+        cvfs << "files" << vcalfiles;
+        cvfs << "points" << vvcal;
+    }
+}
+
+
+
 int main()
 {
-    std::cout << "CPOZ Test Application" << std::endl;
+    std::cout << stitle << std::endl;
     //cpoz::CameraHelper cam;
     //cam.cal(".\\cal_set_2");
     test_room1();
     test_room2();
+    loop_cal();
 }
